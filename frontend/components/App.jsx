c703598bd03c22812fe9718cd59c8b279cb25426
Eric Vo <vo.eric@Erics-MBP.fios-router.home>
2017-06-27 15:06:05 -0400

    Add AuthRoute for splash screen

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { Route, Switch, Link } from 'react-router-dom';
+
 import SessionFormContainer from './session_form/session_form_container';
-import { Route } from 'react-router-dom';
 import HeaderContainer from './header/header_container';
 import ModalContainer from './modal/modal_container';
 import SplashScreenContainer from './splash_screen/splash_screen_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
   <div>
@@ -19,7 +22,7 @@ const App = () => (
         Filler Text <br/>
         Filler Text <br/>
       </p>
-      <SplashScreenContainer />
+      <AuthRoute exact path='/' component={SplashScreenContainer} />
     </section>
   </div>
 );
