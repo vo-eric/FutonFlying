5223b06d18fe89129aa0f11361c1fb313f3e0482
Eric Vo <vo.eric@Erics-MacBook-Pro.local>
2017-06-28 14:04:16 -0400

    Add another portino to drop down

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -8,47 +8,56 @@ class Dashboard extends React.Component {
 
   render () {
     return(
-      <div className='entire-page'>
-        <div className='dashboard'>
+      <div className='dashboard'>
 
-          <div className='dash-user'>
-            <div className='dash-user-info'>
-              <div className='dash-fname'>
-                {this.props.user.fname}
-              </div>
+        <div className='dash-user'>
+          <div className='dash-user-info'>
+            <div className='dash-fname'>
+              {this.props.user.fname}
+            </div>
+          </div>
+        </div>
+
+
+        <div className='trips'>
+
+          <div className='random-locations'>
+            <div className='dash-text'>
+              <i className="fa fa-globe icons" aria-hidden="true"></i>
+              THE WORLD'S FUTONS ARE AT YOUR FINGERTIPS
+            </div>
+
+            <div className='inside-text'>
+              There will be three locations here
             </div>
           </div>
 
-
-          <div className='trips'>
-
-            <div className='travel'>
-              <div className='dash-text'>
-                <i className="fa fa-plane plane-icon" aria-hidden="true"></i>
-                MY TRAVEL PLANS
-              </div>
-
-              <div className='inside-text'>
-                You don't have any upcoming trips.<br/>
-                This is a placeholder.
-              </div>
+          <div className='travel'>
+            <div className='dash-text'>
+              <i className="fa fa-plane icons" aria-hidden="true"></i>
+              MY TRAVEL PLANS
             </div>
 
-            <div className='upcoming-guests'>
-              <div className='dash-text'>
-                <i className="fa fa-home home-icon" aria-hidden="true"></i>
-                  MY UPCOMING GUESTS
-              </div>
+            <div className='inside-text'>
+              You don't have any upcoming trips.<br/>
+              This is a placeholder.
+            </div>
+          </div>
 
-              <div className='inside-text'>
-                You don't have any upcoming guests. <br/>
-                This entire text portion is a placeholder
-              </div>
+          <div className='upcoming-guests'>
+            <div className='dash-text'>
+              <i className="fa fa-home icons" aria-hidden="true"></i>
+                MY UPCOMING GUESTS
             </div>
 
+            <div className='inside-text'>
+              You don't have any upcoming guests. <br/>
+              This entire text portion is a placeholder
+            </div>
           </div>
 
         </div>
+
       </div>
     );
   }
